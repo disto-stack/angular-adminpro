74def9f97ffd51ca50cad70806e3520c7c7b1def
disto-stack <juan.ocampo.2501@gmail.com>
2021-05-18 08:35:11 -0500

    Input decorator

--- src/app/components/increasing/increasing.component.ts
+++ src/app/components/increasing/increasing.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-increasing',
@@ -8,11 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IncreasingComponent {
 
-  progress: number = 50;
-
-  get percentage() {
-    return `${this.progress}%`;
-  }
+  @Input() progress: number = 50;
 
   changeValue(value: number): number | void {
     if (this.progress + value > 100 ) {
