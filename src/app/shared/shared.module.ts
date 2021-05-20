a33ee4167761cfcdebe9e293ab9716ef4dabd763
disto-stack <juan.ocampo.2501@gmail.com>
2021-05-20 11:31:27 -0500

    routerLink to account settings

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { HeaderComponent } from './header/header.component';
 import { BreadcrumsComponent } from './breadcrums/breadcrums.component';
@@ -13,7 +14,8 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     SidebarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     HeaderComponent,
