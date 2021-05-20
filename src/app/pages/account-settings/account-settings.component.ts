6f79a9f8e1de1574d02e47e87e0bc1857e6de06b
disto-stack <juan.ocampo.2501@gmail.com>
2021-05-19 23:29:44 -0500

    Account settings actual theme new feature

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -8,16 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountSettingsComponent implements OnInit {
   private linkTheme = document.querySelector('#theme');
+  private themeLinks: NodeListOf<Element> | undefined;
 
-  constructor() { }
+  constructor() {
+  }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.themeLinks = document.querySelectorAll('.selector');
+    this.checkCurrentTheme();  
+  }
 
   changeTheme(themeClass: string) {
     const url = `./assets/css/colors/${themeClass}.css`
     
     this.linkTheme?.setAttribute('href', url);
     localStorage.setItem('theme', url);
+
+    this.checkCurrentTheme();
   }
 
+  checkCurrentTheme() {    
+    this.themeLinks?.forEach(element => {
+      element.classList.remove('working');
+
+      const bntTheme = element.getAttribute('data-theme');
+      const btnUrl = `./assets/css/colors/${bntTheme}.css`;
+
+      const currentTheme = this.linkTheme?.getAttribute('href');
+
+      if (btnUrl === currentTheme) element.classList.add('working');
+    })
+  }
 }
